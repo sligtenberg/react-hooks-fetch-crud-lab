416407414032de9527ecf359bb5540ff31229598
stephen ligtenberg <bountyhunter24@gmail.com>
2022-12-05 20:54:31 -0500

    _

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDelete }) {
+  const { id, prompt, answers } = question;
+  const [correctAnswerIndex, setCorrectAnswerIndex] = useState(question.correctIndex)
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +10,33 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE"
+    })
+    .then(r => r.json())
+    .then(() => onDelete(question))
+  }
+
+  function handleCorrectAnswerChange(event) {
+    console.log(event.target.value)
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({"correctIndex": event.target.value})
+    })
+    setCorrectAnswerIndex(event.target.value)
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctAnswerIndex} onChange={handleCorrectAnswerChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
